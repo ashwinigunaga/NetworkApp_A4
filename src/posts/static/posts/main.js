93af3146d7eef253026093719f9bfe03e47ab1c1
ashwinigunaga <gunagaashwini@gmail.com>
2025-04-07 19:00:32 -0400

    Making the ajax call

--- src/posts/static/posts/main.js
+++ src/posts/static/posts/main.js
@@ -1,1 +1,16 @@
 console.log('Hello World')
+
+const HelloWorldBox = document.getElementById('hello-world')
+
+$.ajax({
+    type: 'GET',
+    url: '/hello-world/',
+    success: function(response){
+        console.log('success', response.text)
+        HelloWorldBox.textContent = response.text
+    },
+
+    error: function(error){
+        console.log('error', error)
+    }
+})
