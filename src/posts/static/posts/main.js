6833d704af85a47f6504bd5352101e4e55207058
ashwinigunaga <gunagaashwini@gmail.com>
2025-04-07 19:47:06 -0400

    Added a spinner

--- src/posts/static/posts/main.js
+++ src/posts/static/posts/main.js
@@ -2,6 +2,7 @@ console.log('Hello World')
 
 const HelloWorldBox = document.getElementById('hello-world')
 const postsBox = document.getElementById('posts-box')
+const spinnerBox = document.getElementById('spinner-box')
 
 $.ajax({
     type: 'GET',
@@ -22,12 +23,16 @@ $.ajax({
     success: function(response){
         console.log(response)
         const data = response.data
-        console.log(data)
-        data.forEach(el => {
-            postsBox.innerHTML += `
-                ${el.title} - <b>${el.body}</b><br>
-            `
-        });
+        setTimeout(()=>{
+            spinnerBox.classList.add('not-visible')
+            console.log(data)
+            data.forEach(el => {
+                postsBox.innerHTML += `
+                    ${el.title} - <b>${el.body}</b><br>
+                `
+            });
+        },1000)
+        
     },
     errror: function(response){
         console.log(error)
